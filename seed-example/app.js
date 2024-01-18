88af03d53178e7af2dcc9bb3b69c59760bd97569
Joshua B. Turner <josh.turner@projekt202.com>
2024-01-18 01:44:20 +0000

    Initial Node.js Express app

---
+++ seed-example/app.js
@@ -0,0 +1,8 @@
+// app.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World from Gitpod!'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
